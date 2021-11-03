a04c604e2a0891c2f5378e0fa3f3ea4d284e3206
Henry Onu <onuhenry24@gmail.com>
2021-11-03 08:46:11 +0100

    Add price helper to helpers

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+    def price(movie)
+        if movie.price == 0
+            "Free"
+        else
+            number_to_currency(movie.price, precision: 0)
+        end
+    end
 end
