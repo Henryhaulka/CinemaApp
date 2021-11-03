cdc0567cc38cb76c7fd5ac9320d39ee4ff205eb5
Henry Onu <onuhenry24@gmail.com>
2021-11-03 15:43:03 +0100

    Add movies resources to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get 'movies' => 'movies#index'
+  root  'movies#index'
+  # get 'movies' => 'movies#index'
+  resources :movies
+  # get 'movies/:id' => 'movies#show'
 end
