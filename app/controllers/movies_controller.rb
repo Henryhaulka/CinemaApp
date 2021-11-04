c1f4de6db5680806353b75076e26efbf3d290948
Henry Onu <onuhenry24@gmail.com>
2021-11-04 09:52:42 +0100

    Add create and new actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,20 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+     @movie = Movie.new(movie_params)
+     if @movie.save
+       redirect_to movie_path(@movie)
+     else
+       render :new
+     end
+     
+  end
+  
   def show
     @movie = Movie.find(params[:id])
   end
@@ -10,6 +24,21 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params) 
+      redirect_to movie_path(@movie) 
+    else
+      render :edit
+    end
+  end
+   
+  private
+  def movie_params
+    params.require(:movie).permit(:name, :location, :description, :viewing_date, :price)
+  end
+  
   
   
 end
