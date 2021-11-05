f49e105f121f0bb9775f50bb87fb94c8691a362a
Henry Onu <onuhenry24@gmail.com>
2021-11-05 21:26:08 +0100

    REfactor code

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ApplicationRecord
     mount_uploader :avatar, AvatarUploader
     validates :name, :location, presence: true
     validates :description, length: {minimum: 15}
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+    validates :price, numericality: { greater_than_or_equal_to: 0}
     validates :capacity, numericality: {only_integer: true, greater_than: 0}
     validates :avatar, format: {with: /\w+\.(png|jpg|jpeg)\z/i, message: 'Image should be PNG OR JPG or JPEG format'}
     def self.upcoming
