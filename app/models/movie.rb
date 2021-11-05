bdc196dc641f13803343476bab88ed5c4f7ce820
Henry Onu <onuhenry24@gmail.com>
2021-11-05 15:05:34 +0100

    Add validations to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,10 @@
 class Movie < ApplicationRecord
     mount_uploader :avatar, AvatarUploader
+    validates :name, :location, presence: true
+    validates :description, length: {minimum: 15}
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+    validates :capacity, numericality: {only_integer: true, greater_than: 0}
+    validates :avatar, format: {with: /\w+\.(png|jpg|jpeg)\z/i, message: 'Image should be PNG OR JPG or JPEG format'}
     def self.upcoming
         where('viewing_date > ?', Time.now)
     end
