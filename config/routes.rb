786871f2a7e95f9d75659009a3ca92795e9192f0
Henry Onu <onuhenry24@gmail.com>
2021-11-08 08:40:22 +0100

    Nest registrations in movies for best route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :registrations
+  
   root 'movies#index'
-  resources :movies
+  resources :movies do
+     resources :registrations
+  end
   # resources handles all these below
   # get 'movies' => 'movies#index'
   # get 'movies/new' => 'movies#new'
