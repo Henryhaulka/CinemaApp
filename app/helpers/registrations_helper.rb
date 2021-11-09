d1695fac2ac2fdc7cea132072476bfd7a9b1e730
Henry Onu <onuhenry24@gmail.com>
2021-11-09 10:05:55 +0100

    Add helper for sold out

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,10 +1,12 @@
 module RegistrationsHelper
-    # def register_check(registration)
-    #     if @registration.exists?
-    #     p  "#{registration.name}"
-           
-    #     else
-    #         "NO ONE REGISTERED FOR THIS MOVIE YET"
-    #     end
-    # end  
+    def register_check(movie,registration)
+        if registration.exists?
+            "Registrations for #{@movie.name}" 
+            registration.name
+            
+      
+        else
+            "NO ONE REGISTERED FOR THIS MOVIE YET"
+        end
+    end  
 end
