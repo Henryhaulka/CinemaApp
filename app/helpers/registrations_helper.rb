cc8f76a8101fa72b46158ddd72945773a68636ad
Henry Onu <onuhenry24@gmail.com>
2021-11-09 08:59:11 +0100

    Refactor code

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,10 @@
 module RegistrationsHelper
+    # def register_check(registration)
+    #     if @registration.exists?
+    #     p  "#{registration.name}"
+           
+    #     else
+    #         "NO ONE REGISTERED FOR THIS MOVIE YET"
+    #     end
+    # end  
 end
