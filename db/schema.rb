c81d9bc3c08a879d079147c9a99db7f1376994b6
Henry Onu <onuhenry24@gmail.com>
2021-11-09 12:03:43 +0100

    Generate and add all users resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_214039) do
+ActiveRecord::Schema.define(version: 2021_11_09_090426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 2021_11_05_214039) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "movie_id", null: false
     t.index ["movie_id"], name: "index_registrations_on_movie_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "registrations", "movies"
