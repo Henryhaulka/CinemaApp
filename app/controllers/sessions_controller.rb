d1a74a67852f365cc68e0c3268fc3aa56d4b8dce
Henry Onu <onuhenry24@gmail.com>
2021-11-10 11:18:20 +0100

    Add destroy action to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if !user.nil? && user.authenticate(params[:password])
+      #saving the user.id in a hash which will be use to track the user when signed in
        session[:user_id] = user.id
        redirect_to user_path(user), notice: "Welcome back, #{user.name.upcase} \u{1f600}"
     else
@@ -14,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Bye, You have Successfully signed out"
   end
 end
