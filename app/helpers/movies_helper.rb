c9184f9ce0af80dfadd8de241eac3e1c05672c00
Henry Onu <onuhenry24@gmail.com>
2021-11-14 18:23:38 +0100

    Add conditions for like/unlike , reg/unreg to view

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -15,7 +15,7 @@ module MoviesHelper
 
   def for_avatar_medium(movie)
     if movie.avatar?
-      image_tag @movie.avatar.url(:medium)
+      image_tag movie.avatar.url(:medium) 
     else
       image_tag 'default_avatar.jpg'
     end
@@ -36,4 +36,26 @@ module MoviesHelper
       link_to 'Register !', new_movie_registration_path(movie), class: 'btn btn-success'
     end
   end
+
+  def like_or_unlike(liked, movie)
+      if liked 
+        button_to( "Unfav \u{1f44e}", movie_like_path(movie, liked),
+        method: :delete )
+      else 
+        button_to( "Fav \u{1f44d}", movie_likes_path(movie))
+      end 
+  end
+
+  def registered_or_unregistered(movie, registered )
+     if registered 
+       link_to 'Unregister', movie_registration_path(movie, @registered), 
+        method: :delete, class: 'btn btn-danger', 
+        data: { confirm: "Are you sure you want to unregister for #{movie.name}"}
+    else
+        spot_sold_out(movie)
+    end
+   
+  end
+  
+  
 end
