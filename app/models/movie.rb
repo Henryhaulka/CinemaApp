0622a805ae5afb69a6a8c8b81101f6feb9a133e9
Henry Onu <onuhenry24@gmail.com>
2021-11-15 10:25:47 +0100

    Add scope for upcoming, released free& recent movi

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,9 +11,13 @@ class Movie < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :capacity, numericality: { only_integer: true, greater_than: 0 }
   validates :avatar, format: { with: /\w+\.(png|jpg|jpeg)\z/i, message: 'Image should be PNG OR JPG or JPEG format' }
-  def self.upcoming
-    where('viewing_date > ?', Time.now)
-  end
+  
+  scope :upcoming, lambda { where('viewing_date > ?', Time.now).order('viewing_date')}
+  scope :released, -> { where('viewing_date < ?', Time.now).order('viewing_date')}
+  scope :free, lambda { upcoming.where(price: 0.0).order('name')}
+  scope :recent, -> (max= 3) { released.limit(max) }
+  # Ex:- scope :active, lambda {where(:active => true)}
+ 
 
   def zero?
     self[:price].zero?
