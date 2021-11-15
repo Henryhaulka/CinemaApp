127a7907df0fcb8d459352a57cd3fcab32619580
Henry Onu <onuhenry24@gmail.com>
2021-11-15 15:25:30 +0100

    Add filtered_movies to routes and header

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users
   get 'signup' => 'users#new'
   root 'movies#index'
+  get 'movies/filter/:filter' => 'movies#index', as: :filtered_movies
   resources :movies do
     resources :registrations
     resources :likes, only: [:create, :destroy]
