6e2dccd243e570b4e915b2d580dda4e558370a0d
Henry Onu <onuhenry24@gmail.com>
2021-11-17 09:05:52 +0100

    Generated a comment model and added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :registrations, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_movies, through: :likes, source: :movie
+  has_many :comments, dependent: :destroy
+  has_many :movie_comments, through: :comments, source: :movie
 
   validates :name, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
