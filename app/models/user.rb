62b13de9b0e96432716fa22b8fa1d27e5c0b0be0
Henry Onu <onuhenry24@gmail.com>
2021-11-22 09:46:55 +0100

    Fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
   has_many :liked_movies, through: :likes, source: :movie
   has_many :comments, dependent: :destroy
   has_many :movie_comments, through: :comments, source: :movie
-  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: 'default_avatar.jpg'
+  has_attached_file :image, storage: :cloudinary,
+                             path: ':id/:style/:filename',
+                             styles: { large: '1200x1000', medium: '300x300>' },
+                             default_url: 'default_avatar.jpg'
   validates_attachment_content_type :image, content_type: %r{\Aimage/.*\z}
-
   validates :name, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
                     uniqueness: { case_sensitive: false }
