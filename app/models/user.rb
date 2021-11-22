f660a9856307642dd18307e49f6c872bf84d7670
Henry Onu <onuhenry24@gmail.com>
2021-11-22 10:00:39 +0100

    Fix rubocop error

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :movie_comments, through: :comments, source: :movie
   has_attached_file :image, storage: :cloudinary,
-                    path: ':id/:style/:filename',
-                    styles: { large: '1200x1000', medium: '300x300>' },
-                    default_url: 'default_avatar.jpg'
+                            path: ':id/:style/:filename',
+                            styles: { large: '1200x1000', medium: '300x300>' },
+                            default_url: 'default_avatar.jpg'
   validates_attachment_content_type :image, content_type: %r{\Aimage/.*\z}
   validates :name, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
